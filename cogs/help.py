0b2ea51df0b43736289fb08235546208dda1f3c1
jing1201 <low.jing@hotmail.com>
2018-10-04 18:45:54 -0400

    Added the question command to notify the officers whenever a member has a question

---
+++ cogs/help.py
@@ -0,0 +1,26 @@
+import discord
+import random
+from discord.ext import commands
+
+class Help:
+    '''Commands related to help needed'''
+    def __init__(self, bot):
+        self.bot = bot
+    
+    '''
+    Formats:
+    !question
+    !question "[question to be answered]"
+    '''
+    @commands.command()
+    async def question(self, ctx, q:str = ""):
+        '''Forward a question to the officer chat'''
+        member_name = ctx.message.author.name
+        channel = self.bot.get_channel(436916990238785556)
+        await channel.send("@everyone\n @"+member_name+" has a question!")
+        if (q != ""):
+            await channel.send("\""+q+"\"")
+    
+
+def setup(bot):
+    bot.add_cog(Help(bot))
