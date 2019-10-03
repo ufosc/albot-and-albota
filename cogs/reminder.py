a576a23d2ffcddba19521a18071d9c522549172a
Alexander Shuping <alexandershuping@gmail.com>
2019-10-03 11:11:50 -0400

    Improve Casual-Coding Booking reminder (#83)
    * Bump youtube-dl from 2019.6.8 to 2019.6.21
    
    Bumps [youtube-dl](https://github.com/ytdl-org/youtube-dl) from 2019.6.8 to 2019.6.21.
    - [Release notes](https://github.com/ytdl-org/youtube-dl/releases)
    - [Changelog](https://github.com/ytdl-org/youtube-dl/blob/master/ChangeLog)
    - [Commits](https://github.com/ytdl-org/youtube-dl/compare/2019.06.08...2019.06.21)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump youtube-dl from 2019.6.21 to 2019.7.2
    
    Bumps [youtube-dl](https://github.com/ytdl-org/youtube-dl) from 2019.6.21 to 2019.7.2.
    - [Release notes](https://github.com/ytdl-org/youtube-dl/releases)
    - [Changelog](https://github.com/ytdl-org/youtube-dl/blob/master/ChangeLog)
    - [Commits](https://github.com/ytdl-org/youtube-dl/compare/2019.06.21...2019.07.02)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump discord-py from 1.1.1 to 1.2.3
    
    Bumps [discord-py](https://github.com/Rapptz/discord.py) from 1.1.1 to 1.2.3.
    - [Release notes](https://github.com/Rapptz/discord.py/releases)
    - [Commits](https://github.com/Rapptz/discord.py/compare/v1.1.1...v1.2.3)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump requests from 2.21.0 to 2.22.0
    
    Bumps [requests](https://github.com/requests/requests) from 2.21.0 to 2.22.0.
    - [Release notes](https://github.com/requests/requests/releases)
    - [Changelog](https://github.com/kennethreitz/requests/blob/master/HISTORY.md)
    - [Commits](https://github.com/requests/requests/compare/v2.21.0...v2.22.0)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump praw from 6.2.0 to 6.3.1
    
    Bumps [praw](https://github.com/praw-dev/praw) from 6.2.0 to 6.3.1.
    - [Release notes](https://github.com/praw-dev/praw/releases)
    - [Changelog](https://github.com/praw-dev/praw/blob/master/CHANGES.rst)
    - [Commits](https://github.com/praw-dev/praw/compare/v6.2.0...v6.3.1)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Rewrite reminder.py
    
    Improve handling of the weekly Casual-Coding room-booking reminder command
    
    * Fix comment
    
    Update time listed in comment to 4PM.
    
    * Remove debug print statements
    
    * Add pygithub to requirements.txt

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,8 +1,11 @@
+import asyncio
+import datetime
 import discord
 import random
-import datetime
-import asyncio
+
 from discord.ext import commands
+
+import cogs.CONSTANTS as CONSTANTS
 
 class Reminder(commands.Cog, name='Reminder'):
     """To remind officers and members about things"""
@@ -11,22 +14,44 @@ class Reminder(commands.Cog, name='Reminder'):
         self.book_room_bg_task = self.bot.loop.create_task(self.book_room_reminder())
     
     async def book_room_reminder(self):
+        """ Every week, on Wednesday at 16:00, this method sends a reminder
+            message to all users in the provided channel, reminding them to
+            book a room for casual coding.
+        """
         await self.bot.wait_until_ready()
+
         channel = self.bot.get_channel(436916990238785556)
+        # NOTE: weekday() returns an int from 0 (monday) to 6 (sunday)
+        trigger_day = 2 # 2 = Wednesday
+
         while True:
-            current_day_of_the_week = (str)(datetime.date.today().strftime("%A"))
-            #print (current_day_of_the_week)
-            if current_day_of_the_week == "Wednesday" or current_day_of_the_week == "Thursday":
-                await channel.send("@everyone Reminder to book rooms!")
-                # ideally, the bot would send the reminder on Wednesdat 12am, 
-                # but to account for when the bot starts running,
-                # the time below is 2 minutes less than 1 week,
-                # this allows the time to be slowly adjusted every week until it reaches 12am
-                await asyncio.sleep(604680) # a little less than a week
-            else:
-                # this line will run twice every week once 
-                # the time is stabilized as mentioned above
-                await asyncio.sleep(60) # check every 1 minute
+            cur_time = datetime.datetime.now()
+            day_delta = trigger_day - cur_time.weekday()
+            if day_delta <= 0:
+                day_delta += 7
+
+            destination_time = cur_time.replace(
+                hour=16,
+                minute=0,
+                second=0,
+                microsecond=0
+            ) + datetime.timedelta(day_delta,0,0)
+
+            embed = discord.Embed(
+                title="{info} Reminder".format(
+                    info=CONSTANTS.REACTION_INFO
+                ),
+                colour=discord.Colour(CONSTANTS.EMBED_COLOR_STANDARD),
+                description="Remember to book rooms for Casual Coding!",
+            )
+
+            embed.set_footer(text="This reminder will repeat at {nxt}.".format(
+                nxt=destination_time.isoformat(" ")
+            ))
+
+
+            await channel.send("@everyone", embed=embed)
+            await asyncio.sleep((destination_time - datetime.datetime.now()).seconds())
 
 def setup(bot):
     bot.add_cog(Reminder(bot))
