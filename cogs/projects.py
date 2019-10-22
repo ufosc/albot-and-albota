464321c8007f21fedd682c07b6053ce028d9ef62
Dakota Rennemann <rennemannd@gmail.com>
2019-10-22 07:55:23 -0400

    Removed MVW from project list and added SiLGA and Seg-Fault (#101)

--- cogs/projects.py
+++ cogs/projects.py
@@ -27,11 +27,17 @@ class Projects(commands.Cog, name='Projects'):
         await ctx.author.add_roles(role)
         await ctx.send("HTML is my favorite programming language.")
 
-    async def mvw(self, ctx):
-        """Add the marston vs west role to the user"""
-        role = get(ctx.guild.roles, name="marston-vs-west")
+    async def fault(self, ctx):
+        """Add the seg-fault role to the user"""
+        role = get(ctx.guild.roles, name="seg-fault")
         await ctx.author.add_roles(role)
-        await ctx.send("Newell is the best 24/7 library. Don't @ me")
+        await ctx.send("Segmentation fault (core dumped)")
+
+    async def graphics(self, ctx):
+        """Add graphics-accelerator role to the user"""
+        role = get(ctx.guild.roles, name="graphics-accelerator")
+        await ctx.author.add_roles(role)
+        await ctx.send("Accelerating... Accelerating... Accelerating...")
 
     @commands.command()
     async def join(self, ctx, *, roleName: str):
@@ -42,8 +48,10 @@ class Projects(commands.Cog, name='Projects'):
             await self.muddy(ctx)
         elif roleName.lower() in ["website", "club site", "club website", "clubwebsite", "clubsite"]:
             await self.website(ctx)
-        elif roleName.lower() in ["mvw", "marstonvswest", "marston vs west", "marston v west"]:
-            await self.mvw(ctx)
+        elif roleName.lower() in ["fault", "seg-fault", "segfault", "seg fault", "seg", "sf", "s-f"]:
+            await self.fault(ctx)
+        elif roleName.lower() in ["graphics", "graphics accelerator", "graphics-accelerator", "g-a", "accelerator", "silga"]:
+            await self.graphics(ctx)
 
     @commands.command()
     async def list(self, ctx):
@@ -58,6 +66,14 @@ class Projects(commands.Cog, name='Projects'):
 
         embed=discord.Embed(title="Club Website", url="https://github.com/ufosc/club-website", description="Our club website made using basic HTML, CSS, and JS.", color=0x00ecff)
         embed.add_field(name="Join using", value="!join clubsite", inline=True)
+        await ctx.send(embed=embed)
+
+        embed = discord.Embed(title="Seg Fault", url="https://github.com/ufosc/seg-fault", description="A python based discord bot", color=0xff0036)
+        embed.add_field(name="Join using", value="!join fault", inline=True)
+        await ctx.send(embed=embed)
+
+        embed = discord.Embed(title="Graphics Accelerator", url="https://github.com/ufosc/SiLGA", description="Simple Lightweight Graphics Accelerator (SiLGA)", color=0x7F6000)
+        embed.add_field(name="Join using", value="!join graphics", inline=True)
         await ctx.send(embed=embed)
 
 def setup(bot):
